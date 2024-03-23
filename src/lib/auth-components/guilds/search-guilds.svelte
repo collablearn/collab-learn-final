b734f4d0f6670e5b0136c4817502e6b797c04cdb
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-24 02:09:55 +0800

    add search component

--- src/lib/auth-components/guilds/search-guilds.svelte
+++ src/lib/auth-components/guilds/search-guilds.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
+	import seachIcon from '$lib/assets/guilds_search_icon_320.svg';
 </script>
 
-<input
-	placeholder="Search Guild Names"
-	type="text"
-	class="outline-none w-full text-[14px] py-[11px] px-[20px] text-main bg-submain border-[1px] border-main rounded-[10px] transition-all"
-/>
+<div class="relative flex items-center">
+	<input
+		placeholder="Search Guild Names"
+		type="text"
+		class="outline-none w-full text-[14px] py-[11px] pl-[20px] pr-[40px] text-main bg-submain border-[1px] border-main rounded-[10px] transition-all"
+	/>
+
+	<div class="absolute right-0 mr-5">
+		<img src={seachIcon} alt="search-icon" class="" />
+	</div>
+</div>
 
 <style>
 	::placeholder {
