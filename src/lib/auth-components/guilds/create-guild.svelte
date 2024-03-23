e38a0e816ab76eeb96e8dfa4886da6c1745e47a2
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-24 00:53:58 +0800

    add create guild btn

--- src/lib/auth-components/guilds/create-guild.svelte
+++ src/lib/auth-components/guilds/create-guild.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+	import createIcon from '$lib/assets/create_guild_icon_320.svg';
 </script>
 
-<div class=""></div>
+<div class="fixed bottom-0 right-0 m-[20px]">
+	<button class="">
+		<img src={createIcon} alt="create-icon" class="" />
+	</button>
+</div>
