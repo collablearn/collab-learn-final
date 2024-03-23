5af6e5404d90ca5127288f3d0f9cb803f2a05c3a
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-24 01:39:51 +0800

    add forms

---
+++ src/lib/auth-components/guilds/create-guild/create-guild-form.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+</script>
+
+<div class="flex flex-col gap-[10px]">
+	<label>
+		<span class="text-main text-[14px] transition-all">Project Name</span>
+		<input
+			type="text"
+			class="outline-none w-full text-[14px] py-[11px] px-[20px] text-main bg-submain border-[1px] border-main rounded-[10px] transition-all"
+		/>
+	</label>
+
+	<label>
+		<span class="text-main text-[14px] transition-all">Users (Max. 50)</span>
+		<input
+			type="text"
+			class="outline-none w-full text-[14px] py-[11px] px-[20px] text-main bg-submain border-[1px] border-main rounded-[10px] transition-all"
+		/>
+	</label>
+
+	<label>
+		<span class="text-main text-[14px] transition-all">Description</span>
+		<textarea
+			class="outline-none w-full text-[14px] py-[11px] px-[20px] text-main bg-submain border-[1px] border-main rounded-[10px] transition-all"
+		/>
+	</label>
+</div>
+
+<div class="mt-[35px]">
+	<h3 class="text-[16px] text-main font-semibold">Guild Visibility</h3>
+</div>
