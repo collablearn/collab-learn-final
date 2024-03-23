05688100894a8eda143284a8d7bb320edefefc2c
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-23 19:24:26 +0800

    final touch for 320 breakpoints

--- src/routes/(authenticated)/dashboard/+page.svelte
+++ src/routes/(authenticated)/dashboard/+page.svelte
@@ -1,45 +1,4 @@
 <script lang="ts">
 </script>
 
-<div class="">
-	Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nisi vero repudiandae, hic qui dolores
-	perferendis nesciunt ducimus, facere id fuga ad maiores eos, iure quia est! Iusto doloribus,
-	voluptatem commodi error sit blanditiis dolorem nostrum odio voluptatibus, quas possimus aperiam
-	itaque ipsum officiis, quis est quos magni reiciendis in provident. Quisquam, natus? Illo
-	veritatis nam repellat laudantium eius fugit hic fuga, rem, quibusdam asperiores ex maiores.
-	Recusandae fugiat sequi corrupti beatae veniam nesciunt quod accusamus laudantium iure! Veritatis
-	vitae sint nesciunt! Porro fuga sequi doloremque tempore enim illum velit ipsum aliquam voluptate
-	nam asperiores, voluptatem reprehenderit quam eveniet dolore, assumenda dolor sit magnam est
-	exercitationem. Reprehenderit cumque facilis hic laboriosam necessitatibus dolore accusamus magni
-	sequi fugit? Praesentium vel, assumenda dolore amet quae natus rem nulla obcaecati incidunt iure
-	facilis odit nemo ratione corrupti quos aliquid delectus sit aspernatur sequi quisquam itaque
-	commodi dolor eaque. Reiciendis vel quod, incidunt a aliquid necessitatibus omnis perferendis
-	saepe sunt ut ullam, nostrum beatae sapiente quasi laudantium quam, id molestiae eum aliquam
-	provident. Cum corporis, quaerat maxime quod amet, repellat itaque impedit ullam, tenetur
-	doloribus fugiat facere vero. Sed enim, ullam odio animi nisi fugiat corrupti, itaque voluptatem
-	atque autem, ut dolores? Asperiores pariatur consequatur explicabo veniam voluptatibus veritatis
-	aut sapiente rerum dolorem possimus placeat, eligendi porro tempore iure nesciunt dolore! Impedit
-	facere quos earum aliquid saepe optio, nihil perferendis molestiae! Voluptas, impedit, deleniti
-	dolore soluta blanditiis odio architecto repellendus, fugit magni neque exercitationem numquam
-	iusto. Dignissimos reprehenderit labore at, exercitationem aut ea cum voluptatem consectetur est
-	veritatis unde! Suscipit non exercitationem dolorem ipsam enim placeat tenetur dolore similique
-	nulla, cupiditate quidem sed! Totam, repellendus aut dolorem iure quod nemo ipsum earum quisquam
-	esse suscipit accusantium minima ad, eligendi accusamus nihil molestiae corporis, est nisi cum
-	praesentium beatae exercitationem assumenda. Voluptate totam cum, distinctio impedit fugit iure!
-	Incidunt, optio temporibus aut esse nesciunt doloremque autem unde exercitationem tempore
-	accusamus dolores asperiores recusandae voluptatum enim perferendis ipsam iste ipsum itaque quia
-	laboriosam et debitis! Consequatur alias iure ex temporibus esse porro quisquam exercitationem quo
-	a delectus cum sit dignissimos laborum natus vitae, quae fuga harum nisi. Earum nulla, temporibus
-	laudantium enim necessitatibus pariatur aut beatae odio laborum voluptatum molestias sint
-	assumenda. Ipsa, repellendus eum sunt esse commodi nihil rem labore earum reprehenderit impedit
-	incidunt quae. Fugiat eveniet, at quaerat in repellendus exercitationem repudiandae ducimus rem
-	quis cumque voluptatibus autem, rerum molestias eligendi dolorum eaque odio! Omnis tempore
-	temporibus eius recusandae architecto ad et, tempora quae beatae consequatur cum laborum enim eum
-	possimus! Sequi laboriosam incidunt repellendus, aperiam ea pariatur facilis placeat molestias
-	quae ratione inventore voluptas obcaecati a perspiciatis delectus illo voluptatum mollitia nihil
-	atque modi. Exercitationem eius quis eligendi a sed quaerat tenetur quam eum fugit atque?
-	Cupiditate, amet quia. Aspernatur omnis atque corrupti, voluptas maxime sapiente harum aut
-	expedita neque consequatur velit asperiores veritatis nam, temporibus impedit minima vero aliquam,
-	doloribus placeat. Eveniet, explicabo. Et hic vitae dolorem natus eum rem iure fugiat quasi dicta
-	saepe, illo quae quo quibusdam, quidem asperiores deserunt?
-</div>
+<div class=""></div>
