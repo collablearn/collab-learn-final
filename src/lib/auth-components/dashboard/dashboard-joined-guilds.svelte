2aad281c4fbbcf87bc30ab854493c53fbc3d9d20
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-23 19:53:10 +0800

    add guild card

---
+++ src/lib/auth-components/dashboard/dashboard-joined-guilds.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<div class="">
+	<p class="text-[16px] text-main font-semibold w-full">Joined Guild(s)</p>
+
+	<hr class="mt-[11px] mb-[24px] w-full border-[1px] border-main" />
+
+	<div class="text-main min-h-[140px] flex justify-center items-center">
+		<p class="text-[14px]">You have no guild yet.</p>
+	</div>
+</div>
--- src/routes/(authenticated)/dashboard/+page.svelte
+++ src/routes/(authenticated)/dashboard/+page.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
+	import DashboardJoinedGuilds from '$lib/auth-components/dashboard/dashboard-joined-guilds.svelte';
 	import DashboardRecentProjects from '$lib/auth-components/dashboard/dashboard-recent-projects.svelte';
 </script>
 
-<div class="">
+<div class="pt-[50px] px-[22px]">
 	<div class="">
 		<DashboardRecentProjects />
+	</div>
+
+	<div class="mt-[60px]">
+		<DashboardJoinedGuilds />
 	</div>
 </div>
