7ced50664f32e366d18669f6bb1232f7dfbb936b
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-23 22:22:28 +0800

    add change password

--- src/lib/auth-components/edit-profile/change-password.svelte
+++ src/lib/auth-components/edit-profile/change-password.svelte
@@ -1,1 +1,24 @@
-change pass
+<script lang="ts">
+</script>
+
+<form class="flex flex-col gap-[10px]">
+	<label>
+		<span class="text-main text-[14px] transition-all">New Password</span>
+		<input
+			type="password"
+			class="outline-none text-[14px] py-[11px] px-[20px] text-main bg-submain border-[1px] border-main rounded-[10px] transition-all"
+		/>
+	</label>
+
+	<label>
+		<span class="text-main text-[14px] transition-all">Confirm New Password</span>
+		<input
+			type="password"
+			class="outline-none text-[14px] py-[11px] px-[20px] text-main bg-submain border-[1px] border-main rounded-[10px] transition-all"
+		/>
+	</label>
+	<button
+		class="py-[11px] font-semibold text-[14px] flex items-center justify-center bg-main rounded-[10px] text-white"
+		>Update Password
+	</button>
+</form>
