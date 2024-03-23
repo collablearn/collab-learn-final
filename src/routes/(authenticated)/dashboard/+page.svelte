990897bdee55a1f8c1d8320d0b5b2ccf0bfa21d6
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-23 19:49:01 +0800

    add projects card

--- src/routes/(authenticated)/dashboard/+page.svelte
+++ src/routes/(authenticated)/dashboard/+page.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+	import DashboardRecentProjects from '$lib/auth-components/dashboard/dashboard-recent-projects.svelte';
 </script>
 
-<div class=""></div>
+<div class="">
+	<div class="">
+		<DashboardRecentProjects />
+	</div>
+</div>
