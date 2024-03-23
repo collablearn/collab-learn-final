74400343c4e3aad0b7d7e4775f39083c0f10da6c
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-23 16:26:34 +0800

    add icons and logout component

---
+++ src/lib/auth-components/navigation/user-modal.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import bellIcon from '$lib/assets/bell_icon_320.svg';
+	import feedBackIcon from '$lib/assets/feedback_icon_320.svg';
+	import exitIcon from '$lib/assets/exit_icon_320.svg';
+</script>
+
+<div class=""></div>
