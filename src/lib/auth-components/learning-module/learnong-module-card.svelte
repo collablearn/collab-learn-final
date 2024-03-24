6d213c9c0827c16f62b718bc237b827b65211cec
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-24 23:00:43 +0800

    add click content of uploaded module

--- src/lib/auth-components/learning-module/learnong-module-card.svelte
+++ src/lib/auth-components/learning-module/learnong-module-card.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import learningModIcon from '$lib/assets/learning_mod_icon_320.svg';
+	import Loader from '$lib/general-components/loader.svelte';
 
 	let showLearningContent = false;
 </script>
@@ -26,7 +27,29 @@
 				>
 			</div>
 
-			<div class=""></div>
+			<div class="mt-[90px]">
+				<label>
+					<span class="text-main text-[14px] transition-all">Add Comment</span>
+					<textarea
+						class="outline-none w-full text-[14px] py-[11px] px-[20px] text-main bg-submain border-[1px] border-main rounded-[10px] transition-all"
+					/>
+				</label>
+			</div>
+
+			<div class="flex flex-col gap-[10px] mt-[10px]">
+				<button
+					class="bg-main w-full rounded-[10px] text-[14px] font-semibold py-[10px] px-[2px] flex items-center justify-center text-submain"
+				>
+					<Loader name="Create" txtColor="text-submain text-[14px]" />
+				</button>
+
+				<button
+					on:click={() => (showLearningContent = false)}
+					class="bg-submain text-main w-full rounded-[10px] text-[14px] font-semibold py-[10px] px-[2px] flex items-center justify-center border-[1px] border-main"
+				>
+					Back
+				</button>
+			</div>
 		</div>
 	{:else}
 		<button
