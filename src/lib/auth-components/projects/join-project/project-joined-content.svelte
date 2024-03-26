8eaecb65641a1aa1f0515c6a67ac1a77cab9dc54
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-26 11:15:35 +0800

    add buttons

---
+++ src/lib/auth-components/projects/join-project/project-joined-content.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import projectsBackIcon from '$lib/assets/projects_back_icon_320.svg';
+	import projectAttachIcon from '$lib/assets/project_attach_icon_320.svg';
+	import projectShareScreenIcon from '$lib/assets/project_sharescreen_icon_320.svg';
+	import projectSettingsIcon from '$lib/assets/projects_settings_icon_320.svg';
+</script>
+
+<div class="">
+	<div class="bg-main p-[20px] flex items-center justify-between">
+		<button>
+			<img src={projectsBackIcon} alt="project-back-icon" />
+		</button>
+
+		<div class="flex items-center gap-[20px]">
+			<button>
+				<img src={projectAttachIcon} alt="project-attach-icon" />
+			</button>
+
+			<button>
+				<img src={projectShareScreenIcon} alt="project-sharescreen-icon" />
+			</button>
+
+			<button>
+				<img src={projectSettingsIcon} alt="project-settings-icon" />
+			</button>
+		</div>
+	</div>
+</div>
--- src/routes/(authenticated)/+layout.svelte
+++ src/routes/(authenticated)/+layout.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
 	import Nav from '$lib/auth-components/nav.svelte';
 	import { getActiveItem, setActiveItem } from '$lib';
+	import ProjectJoinedContent from '$lib/auth-components/projects/join-project/project-joined-content.svelte';
 
 	setActiveItem('/dashboard');
 </script>
 
 <div class=" bg-submain">
-	<div class="sticky top-0 z-20">
-		<Nav />
-	</div>
+	{#if true}
+		<ProjectJoinedContent />
+	{:else}
+		<div class="sticky top-0 z-20">
+			<Nav />
+		</div>
 
-	<div class="min-h-screen px-[22px] py-[50px]">
-		<slot />
-	</div>
+		<div class="min-h-screen px-[22px] py-[50px]">
+			<slot />
+		</div>
+	{/if}
 </div>
