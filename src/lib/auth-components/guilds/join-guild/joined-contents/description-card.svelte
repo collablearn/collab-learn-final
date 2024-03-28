456bc2cf9356b1a49e997d50ccc420e1c53eefe6
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-28 15:47:13 +0800

    update icons

---
+++ src/lib/auth-components/guilds/join-guild/joined-contents/description-card.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import sampleIcon from '$lib/assets/description_icon_320_sample.svg';
+</script>
+
+<div class="bg-main flex p-[10px] gap-[10px]">
+	<div class="w-full">
+		<img src={sampleIcon} alt="sample-icon" class="" />
+	</div>
+
+	<div class="flex flex-col">
+		<p class="text-submain text-[14px] font-semibold">ZAYN</p>
+		<hr class="w-full border-[1px] border-submain my-[5px]" />
+		<p class="text-submain text-[14px]">
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident hic minus nulla rem
+			dolorum quam distinctio culpa laudantium, eum ad in inventore incidunt reprehenderit
+			voluptates cumque nam perferendis perspiciatis corrupti. Nulla soluta animi placeat, alias
+			eveniet, perspiciatis quis aperiam ex nobis nihil consectetur unde, quae totam nisi facilis
+			amet laborum tenetur. Est quos saepe numquam earum ad at sapiente ipsum.
+		</p>
+	</div>
+</div>
---
+++ src/lib/auth-components/guilds/join-guild/joined-contents/description.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import DescriptionCard from './description-card.svelte';
+	import createIcon from '$lib/assets/create_guild_icon_320.svg';
+
+	const selections = ["Guild's Wall", 'Chat Feed'];
+</script>
+
+<div class="fixed bottom-0 right-0 mr-[33px] mb-[33px] z-20">
+	<button class="transition-all active:scale-105">
+		<img src={createIcon} alt="add-description-icon" />
+	</button>
+</div>
+
+<div class="mt-[40px]">
+	<div class="flex items-center gap-[10px]">
+		{#each selections as selection}
+			<button class="text-main text-[14px]">{selection}</button>
+		{/each}
+	</div>
+
+	<div class="mt-[20px] flex flex-col gap-[15px]">
+		{#each Array(15) as sample}
+			<DescriptionCard />
+		{/each}
+	</div>
+</div>
