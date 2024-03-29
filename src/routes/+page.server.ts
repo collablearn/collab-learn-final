540c71bb3425cc7351e9a2e09d032737c0d13987
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-29 11:41:47 +0800

    add server validation

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,17 @@
-import type { Actions } from "@sveltejs/kit";
+import { loginSchema } from "$lib/schema";
+import { fail, type Actions } from "@sveltejs/kit";
+import type { ZodError } from "zod";
 
 export const actions: Actions = {
-    loginAction: async ({ locals: { supabase } }) => {
+    loginAction: async ({ locals: { supabase }, request }) => {
+        const formData = Object.fromEntries(await request.formData());
 
+        try {
+            const result = loginSchema.parse(formData);
+        } catch (error) {
+            const zodError = error as ZodError;
+            const { fieldErrors } = zodError.flatten();
+            return fail(400, { errors: fieldErrors });
+        }
     }
 };
