7b295d60afe9776ce934a9f3957528b254c57329
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-31 21:29:39 +0800

    add schema

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -26,6 +26,10 @@ export const registerSchema = z.object({
 
 });
 
+export const resetPasswordSchema = z.object({
+    email: z.string().email()
+})
+
 export const updateInformationSchema = z.object({
     bio: z.string().min(5, { message: "Must enter a valid bio." }),
     firstName: z.string().min(1, { message: "Must enter a valid first name" }),
