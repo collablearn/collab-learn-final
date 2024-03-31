3ef83fbf3b1134a8f9f171c89b13e759eb7a0898
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-31 10:56:01 +0800

    add types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -23,3 +23,19 @@ export type GuildTypes = {
     isPrivate: boolean
     joinedCount: number
 }
+
+//user_list_tb
+export type UserReference = {
+    id: number
+    created_at: string
+    user_id: string
+    user_email: string
+    user_fullname: string
+    user_photo_link: string
+    user_bio: string
+    user_address: string
+    user_barangay: string
+    user_city: string
+    user_religion: string
+    user_contact: string
+}
