23e6bf7694598f7f1a40936e9cbe84448faf792b
itsmikesharescode <mikejohneviota@gmail.com>
2024-03-31 10:57:49 +0800

    update backend

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,6 @@
 import type { PostgrestResponse } from '@supabase/supabase-js';
 import type { LayoutServerLoad } from './$types'
+import type { UserReference } from '$lib/types';
 
 export const load: LayoutServerLoad = async ({ locals: { getSession, supabase } }) => {
 
@@ -7,6 +8,6 @@ export const load: LayoutServerLoad = async ({ locals: { getSession, supabase } 
 
     return {
         session: await getSession(),
-        userData: await supabase.from("user_list_tb").select("*").eq("user_id", session?.user.id) as PostgrestResponse<any[]>
+        userData: await supabase.from("user_list_tb").select("*").eq("user_id", session?.user.id).limit(1).single() as PostgrestResponse<UserReference>
     }
 }
