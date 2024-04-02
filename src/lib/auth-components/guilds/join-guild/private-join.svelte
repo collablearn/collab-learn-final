c40c35b2991f5e603b84dcbc13d25786e67d6df2
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-02 19:15:18 +0800

    add private join UI

--- src/lib/auth-components/guilds/join-guild/private-join.svelte
+++ src/lib/auth-components/guilds/join-guild/private-join.svelte
@@ -1,15 +1,31 @@
 <script lang="ts">
-	import type { GuildTypes } from '$lib/types';
 	import groupIcon from '$lib/assets/guild_group_icon_320.svg';
 	import { fade, scale } from 'svelte/transition';
 	import Loader from '$lib/general-components/loader.svelte';
-
 	import { getAuthState } from '$lib';
-	export let guildObj: GuildTypes;
+	import type { CreatedGuildReference } from '$lib/types';
+	import { toast } from 'svelte-sonner';
+
+	export let guildObj: CreatedGuildReference;
 
 	export let showPrivateJoin = false;
 
 	const authState = getAuthState();
+
+	let password = '';
+
+	//this is insecure way of joining will rethink soon for database calls
+	const handleJoin = () => {
+		if (password === guildObj.passcode) {
+			toast.success('Joining Guild', { description: 'Joined Successfully' });
+			$authState.guilds.guildObj = guildObj;
+			$authState.guilds.joinedGuild = true;
+		} else {
+			toast.error('Joining Guild', {
+				description: `The Passcode is incorrect unable to join ${guildObj.guild_name}`
+			});
+		}
+	};
 </script>
 
 {#if showPrivateJoin}
@@ -18,19 +34,20 @@
 	>
 		<div class="bg-submain py-[50px] px-[22px] w-full relative" in:scale out:fade>
 			<div class="flex flex-col gap-[10px]">
-				<h3 class="text-[24px] text-main">{guildObj.guildName}</h3>
-				<p class="text-[14px] text-main">{guildObj.guildDescription}</p>
+				<h3 class="text-[24px] text-main">{guildObj.guild_name}</h3>
+				<p class="text-[14px] text-main">{guildObj.description}</p>
 			</div>
 
 			<div class="flex items-center gap-[5px] absolute top-0 right-0 m-[20px]">
 				<img src={groupIcon} alt="group-icon" />
-				<p class="text-[14px] text-main">{guildObj.joinedCount}/{guildObj.maxUsers}</p>
+				<p class="text-[14px] text-main">{guildObj.joined_count}/{guildObj.max_users}</p>
 			</div>
 
 			<div class="mt-[20px]">
 				<label>
 					<span class="text-main text-[14px] transition-all">Passcode</span>
 					<input
+						bind:value={password}
 						type="password"
 						class="outline-none w-full text-[14px] py-[11px] px-[20px] text-main bg-submain border-[1px] border-main rounded-[10px] transition-all"
 					/>
@@ -39,13 +56,10 @@
 
 			<div class="mt-[30px] flex flex-col gap-[10px]">
 				<button
-					on:click={() => {
-						$authState.guilds.guildObj = guildObj;
-						$authState.guilds.joinedGuild = true;
-					}}
-					class="bg-main w-full rounded-[10px] text-[14px] font-semibold py-[10px] px-[2px] flex items-center justify-center text-submain"
+					on:click={handleJoin}
+					class="active:bg-main/50 bg-main w-full rounded-[10px] text-[14px] font-semibold py-[10px] px-[2px] flex items-center justify-center text-submain"
 				>
-					<Loader name="Join" txtColor="text-submain text-[14px]" />
+					Join
 				</button>
 
 				<button
