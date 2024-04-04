4a5392de9dfec5dfde25a28fbadcf076c680c1b7
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-04 20:32:02 +0800

    add server actions

--- src/routes/(authenticated)/guilds/+page.server.ts
+++ src/routes/(authenticated)/guilds/+page.server.ts
@@ -1,4 +1,7 @@
+import type { ZodError } from "zod";
 import type { Actions, PageServerLoad } from "./$types";
+import { fail } from "@sveltejs/kit";
+import { addNoteSchema } from "$lib/schema";
 
 
 export const load: PageServerLoad = async ({ locals: { supabase, isLogged }, request }) => {
@@ -8,6 +11,14 @@ export const load: PageServerLoad = async ({ locals: { supabase, isLogged }, req
 
 export const actions: Actions = {
     addNoteAction: async ({ locals: { supabase, isLogged }, request }) => {
+        const formData = Object.fromEntries(await request.formData());
 
+        try {
+            const result = addNoteSchema.parse(formData);
+        } catch (error) {
+            const zodError = error as ZodError;
+            const { fieldErrors } = zodError.flatten();
+            return fail(400, { errors: fieldErrors });
+        }
     }
 };
