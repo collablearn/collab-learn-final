e0daee6af6079c882f6b208e7dfca3931b26f742
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-04 20:52:17 +0800

    add note backend logics

--- src/routes/(authenticated)/guilds/+page.server.ts
+++ src/routes/(authenticated)/guilds/+page.server.ts
@@ -18,12 +18,15 @@ export const actions: Actions = {
             const session = await getSession();
 
             if (session) {
-                /* const { error: addNoteError } = await supabase.from("guild_wall_tb").insert([{
+                const { error: addNoteError } = await supabase.from("guild_wall_tb").insert([{
                     guild_id: result.guildId,
                     user_id: session.user.id,
-                    
-
-                }]) */
+                    user_fullname: result.userFullname,
+                    user_photo_link: result.userPhotoLink,
+                    guild_note: result.guildNote
+                }]);
+                if (addNoteError) return fail(401, { msg: addNoteError.message });
+                else return fail(200, { msg: "Note added." });
             }
 
 
