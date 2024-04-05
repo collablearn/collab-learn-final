1a86ee33395496f599d22b5d696d25a35b58ad55
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-05 14:02:04 +0800

    final touch for dashboard backend

--- src/lib/auth-components/dashboard/dashboard-joined-guilds.svelte
+++ src/lib/auth-components/dashboard/dashboard-joined-guilds.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import arrowRightIcon from '$lib/assets/arrow-right_icon.svg';
 	import { getAuthState } from '$lib';
 
 	const authState = getAuthState();
@@ -9,17 +10,48 @@
 
 	<hr class="mt-[11px] mb-[24px] w-full border-[1px] border-main" />
 
-	<div class="overflow-x-scroll flex space-x-4">
-		{#each $authState.guilds.joinedGuildArray ?? [] as joinGuildObj, index}
-			<div
-				class="flex-shrink-0 w-full h-40 bg-subwhite rounded-lg flex items-center justify-center"
-			>
-				<p>{joinGuildObj.guild_name}</p>
-			</div>
-		{/each}
-	</div>
+	{#if $authState.guilds.joinedGuildArray?.length}
+		<div class="container overflow-x-scroll flex space-x-4">
+			{#each $authState.guilds.joinedGuildArray ?? [] as joinGuildObj, index}
+				<div class="flex-shrink-0 w-full h-40 bg-subwhite rounded-lg p-[20px]">
+					<div class="w-full text-[14px] flex flex-col gap-[10px]">
+						<p class="text-[16px]">Guild Name:</p>
+						<p>{joinGuildObj.guild_name}</p>
+						<p class="text-[16px]">Host Name:</p>
+						<p>{joinGuildObj.host_name}</p>
+					</div>
+				</div>
+			{/each}
+		</div>
 
-	<div class="text-main min-h-[140px] flex justify-center items-center">
-		<p class="text-[14px]">You have no guild yet.</p>
-	</div>
+		{#if $authState.guilds.joinedGuildArray.length > 1}
+			<button class="flex items-center gap-[10px] text-[14px] text-main font-semibold mt-[10px]"
+				>Swipe for more
+				<img src={arrowRightIcon} class="" alt="arrow-right-icon" />
+			</button>
+		{/if}
+	{:else}
+		<div class="text-main min-h-[140px] flex justify-center items-center">
+			<p class="text-[14px]">You have no guild yet.</p>
+		</div>
+	{/if}
 </div>
+
+<style>
+	/* width */
+	::-webkit-scrollbar {
+		width: 10px;
+		height: 5px;
+	}
+
+	/* Handle */
+	::-webkit-scrollbar-thumb {
+		background: #800000;
+		border-radius: 5px;
+	}
+
+	/* Handle on hover */
+	::-webkit-scrollbar-thumb:hover {
+		background: #800000;
+	}
+</style>
