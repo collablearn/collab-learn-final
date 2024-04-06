a4ec8b0fd228bad548b9b480f9b2adf3fe826f42
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-06 20:22:44 +0800

    add server validation

--- src/routes/(authenticated)/APIS/+page.server.ts
+++ src/routes/(authenticated)/APIS/+page.server.ts
@@ -283,7 +283,15 @@ export const actions: Actions = {
 
     ///learning module rout
     uploadModuleAction: async ({ locals: { supabase, safeGetSession }, request }) => {
+        const formData = Object.fromEntries(await request.formData());
 
+        try {
+
+        } catch (error) {
+            const zodError = error as ZodError;
+            const { fieldErrors } = zodError.flatten();
+            return fail(400, { erros: fieldErrors });
+        }
     }
 
 };
