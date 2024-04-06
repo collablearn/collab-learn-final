b8763db0d2ec681927a94c03b949b5fa1976b924
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-06 20:55:37 +0800

    update client UX

--- src/lib/auth-components/learning-module/upload-module/upload-module-form.svelte
+++ src/lib/auth-components/learning-module/upload-module/upload-module-form.svelte
@@ -62,36 +62,52 @@
 	use:enhance={uploadModuleActionNews}
 >
 	<div class="flex flex-col gap-[10px]">
-		<div class="flex justify-end">
-			<div class="flex flex-col gap-[10px]">
-				<label class="max-w-fit">
-					<div
-						class="transition-all active:bg-main/80 cursor-pointer max-w-fit text-[14px] font-semibold h-[40px] rounded-[10px] bg-main text-submain px-[10px] flex items-center"
-					>
-						<div class="flex items-end gap-[10px]">
-							<img src={uploadIcon} alt="upload-icon" />
-							<span>Upload Module</span>
-							<input
-								autocomplete="off"
-								type="file"
-								name="uploadModule"
-								class="hidden"
-								bind:files={file}
-								accept=".pdf, .ppt, .pptx, .doc, .docx, .xls, .xlsx"
-							/>
-						</div>
-					</div>
-				</label>
-
-				{#each formActionError?.uploadModule ?? [] as errMsg}
-					<p class="text-main text-[14px]" in:fade>{errMsg}</p>
-				{/each}
+		{#if file}
+			<div class="w-full" in:fade>
+				<button
+					on:click={() => (file = undefined)}
+					type="button"
+					class=" transition-all w-full active:bg-main/50 justify-between text-[14px] text-submain px-[20px] rounded-[10px] font-semibold bg-main h-[40px] flex items-center gap-[10px]"
+				>
+					<p class="truncate">
+						{file[0].name}
+					</p>
+					<div class="">Close</div>
+				</button>
 			</div>
-		</div>
+		{:else}
+			<div class="flex items-end" in:fade>
+				<div class="flex flex-col gap-[10px]">
+					<label class="max-w-fit">
+						<div
+							class="transition-all active:bg-main/80 cursor-pointer max-w-fit text-[14px] font-semibold h-[40px] rounded-[10px] bg-main text-submain px-[10px] flex items-center"
+						>
+							<div class="flex items-end gap-[10px]">
+								<img src={uploadIcon} alt="upload-icon" />
+								<span>Upload Module</span>
+								<input
+									disabled={uploadModuleLoader}
+									autocomplete="off"
+									type="file"
+									name="uploadModule"
+									class="hidden"
+									bind:files={file}
+									accept=".pdf, .ppt, .pptx, .doc, .docx, .xls, .xlsx"
+								/>
+							</div>
+						</div>
+					</label>
 
+					{#each formActionError?.uploadModule ?? [] as errMsg}
+						<p class="text-main text-[14px]" in:fade>{errMsg}</p>
+					{/each}
+				</div>
+			</div>
+		{/if}
 		<label>
 			<span class="text-main text-[14px] transition-all">Module Title</span>
 			<input
+				disabled={uploadModuleLoader}
 				name="moduleName"
 				type="text"
 				class="outline-none w-full text-[14px] py-[11px] px-[20px] text-main bg-submain border-[1px] border-main rounded-[10px] transition-all"
@@ -104,6 +120,7 @@
 		<label>
 			<span class="text-main text-[14px] transition-all">Description</span>
 			<textarea
+				disabled={uploadModuleLoader}
 				name="description"
 				class="outline-none w-full text-[14px] py-[11px] px-[20px] text-main bg-submain border-[1px] border-main rounded-[10px] transition-all"
 			/>
