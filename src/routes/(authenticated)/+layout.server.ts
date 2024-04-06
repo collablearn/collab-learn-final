bb51fcfb260f9f4f87c937b42ae56772e8e4dafa
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-07 01:34:05 +0800

    add server data fetching

--- src/routes/(authenticated)/+layout.server.ts
+++ src/routes/(authenticated)/+layout.server.ts
@@ -1,7 +1,7 @@
 import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 import type { PostgrestSingleResponse } from "@supabase/supabase-js";
-import type { CreatedGuildReference, CreatedProjectReference, JoinedGuildReference, UserReference } from "$lib/types";
+import type { CreatedGuildReference, CreatedModuleReference, CreatedProjectReference, JoinedGuildReference, UserReference } from "$lib/types";
 
 export const load: LayoutServerLoad = async ({ locals: { supabase, safeGetSession } }) => {
     const { user } = await safeGetSession();
@@ -12,7 +12,8 @@ export const load: LayoutServerLoad = async ({ locals: { supabase, safeGetSessio
         userData: await supabase.from("user_list_tb").select("*").eq("user_id", user.id).limit(1).single() as PostgrestSingleResponse<UserReference>,
         createdGuilds: await supabase.from("created_guild_tb").select("*").order('id', { ascending: false }) as PostgrestSingleResponse<CreatedGuildReference[]>,
         joinedGuilds: await supabase.from("joined_guild_tb").select("*").eq("user_id", user.id).order('id', { ascending: false }) as PostgrestSingleResponse<JoinedGuildReference[]>,
-        createdProjects: await supabase.from("created_projects_tb").select("*").order('id', { ascending: false }) as PostgrestSingleResponse<CreatedProjectReference[]>
+        createdProjects: await supabase.from("created_projects_tb").select("*").order('id', { ascending: false }) as PostgrestSingleResponse<CreatedProjectReference[]>,
+        createdModules: await supabase.from("created_module_tb").select("*").order('id', { ascending: false }) as PostgrestSingleResponse<CreatedModuleReference[]>
     }
 
 };
