1539553d9dab228916ee88d23f8253f7361eca50
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-06 15:57:51 +0800

    update

--- src/lib/auth-components/learning-module/upload-module/upload-module-form.svelte
+++ src/lib/auth-components/learning-module/upload-module/upload-module-form.svelte
@@ -1,8 +1,32 @@
 <script lang="ts">
 	import { fade } from 'svelte/transition';
+	import uploadIcon from '$lib/assets/upload_icon.svg';
+
+	let file: FileList;
 </script>
 
 <div class="flex flex-col gap-[10px]">
+	<div class={file ? 'hidden' : ''}>
+		<label>
+			<div
+				class="transition-all active:bg-main/80 cursor-pointer max-w-fit text-[14px] font-semibold h-[40px] rounded-[10px] bg-main text-submain px-[10px] flex items-center"
+			>
+				<div class="flex items-end gap-[10px]">
+					<img src={uploadIcon} alt="upload-icon" />
+					<span>Upload Module</span>
+					<input
+						autocomplete="off"
+						type="file"
+						name="uploadProfile"
+						class="hidden"
+						bind:files={file}
+						accept=".png, .jpg, .jpeg"
+					/>
+				</div>
+			</div>
+		</label>
+	</div>
+
 	<label>
 		<span class="text-main text-[14px] transition-all">Module Title</span>
 		<input
@@ -41,3 +65,12 @@
 		Back
 	</a>
 </div>
+
+<style>
+	input {
+		-webkit-user-select: none; /* Safari */
+		-moz-user-select: none; /* Firefox */
+		-ms-user-select: none; /* IE/Edge */
+		user-select: none; /* Standard syntax */
+	}
+</style>
