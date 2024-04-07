3d376c1d8097303045e187b1f71c89ab88519520
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-07 18:13:54 +0800

    add delete module guard

--- src/lib/auth-components/learning-module/commenting/add-comment.svelte
+++ src/lib/auth-components/learning-module/commenting/add-comment.svelte
@@ -2,23 +2,25 @@
 	import type { SupabaseClient } from '@supabase/supabase-js';
 </script>
 
-<div class="">
-	<label>
-		<span class="text-main text-[14px] transition-all">Add Comment</span>
-		<textarea
-			name="commentValue"
-			placeholder="Say something..."
-			class="outline-none w-full text-[14px] py-[11px] px-[20px] text-main bg-submain border-[1px] border-main rounded-[10px] transition-all"
-		/>
-	</label>
-</div>
+<form>
+	<div class="">
+		<label>
+			<span class="text-main text-[14px] transition-all">Add Comment</span>
+			<textarea
+				name="commentValue"
+				placeholder="Say something..."
+				class="outline-none w-full text-[14px] py-[11px] px-[20px] text-main bg-submain border-[1px] border-main rounded-[10px] transition-all"
+			/>
+		</label>
+	</div>
 
-<button
-	type="submit"
-	class="transition-all active:bg-main/50 bg-main w-full rounded-[10px] text-[14px] font-semibold py-[10px] px-[2px] flex items-center justify-center text-submain"
->
-	Add Comment
-</button>
+	<button
+		type="submit"
+		class="transition-all active:bg-main/50 bg-main w-full rounded-[10px] text-[14px] font-semibold py-[10px] px-[2px] flex items-center justify-center text-submain"
+	>
+		Add Comment
+	</button>
+</form>
 
 <style>
 	::placeholder {
