29fe7b76f5afd4dcf70c30749bcff85b2be3b8e4
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-12 16:06:15 +0800

    patch guild creator doesnt joined by default

--- src/lib/auth-components/guilds/guild-card.svelte
+++ src/lib/auth-components/guilds/guild-card.svelte
@@ -53,81 +53,78 @@
 	};
 </script>
 
-<div class="w-full">
+<form
+	method="post"
+	action="/APIS?/checkIfJoinedAction"
+	enctype="multipart/form-data"
+	use:enhance={checkIfJoinedActionNews}
+	class="w-full h-full"
+>
+	<input name="guildId" type="hidden" class="hidden" value={guildObj.id} />
 	{#if guildObj.is_private}
 		<PrivateJoin bind:showPrivateJoin {guildObj} />
 	{:else}
 		<PublicJoin bind:showPublicJoin {guildObj} />
 	{/if}
-
-	<form
-		method="post"
-		action="/APIS?/checkIfJoinedAction"
-		enctype="multipart/form-data"
-		use:enhance={checkIfJoinedActionNews}
+	<button
+		type="submit"
+		disabled={checkIfJoinLoader}
+		class="bg-subwhite h-full w-full mb-[20px] p-[20px] rounded-[10px] relative shadow-sm shadow-black text-left flex flex-col gap-[10px] break-words"
 	>
-		<input name="guildId" type="hidden" class="hidden" value={guildObj.id} />
-
-		<button
-			type="submit"
-			disabled={checkIfJoinLoader}
-			class="bg-subwhite w-full p-[20px] rounded-[10px] relative shadow-sm shadow-black text-left flex flex-col gap-[10px] break-words"
-		>
-			{#if checkIfJoinLoader}
-				<div
-					class="absolute left-0 right-0 top-0 bottom-0 bg-[#0000009a] flex flex-col items-center justify-center rounded-[10px] z-10"
-				>
-					<div class="flex flex-col items-center gap-[20px] text-submain">
-						<div
-							class="w-10 h-10 border-[5px] border-b-submain rounded-full animate-spin border-[#0000009a]"
-						></div>
-						<p class="font-bold">Checking if joined</p>
-					</div>
+		{#if checkIfJoinLoader}
+			<div
+				class="absolute left-0 right-0 top-0 bottom-0 bg-[#0000009a] flex flex-col items-center justify-center rounded-[10px] z-10"
+			>
+				<div class="flex flex-col items-center gap-[20px] text-submain">
+					<div
+						class="w-10 h-10 border-[5px] border-b-submain rounded-full animate-spin border-[#0000009a]"
+					></div>
+					<p class="font-bold">Checking if joined</p>
 				</div>
-			{/if}
+			</div>
+		{/if}
 
+		<div class="">
+			<h3 class="text-[16px] text-main font-semibold line-clamp-1">
+				{guildObj.guild_name}
+			</h3>
+		</div>
+		<div class="flex flex-col sm:flex-row gap-[20px]">
+			<!--For guild image-->
 			<div class="">
-				<h3 class="text-[16px] text-main font-semibold line-clamp-1">
-					{guildObj.guild_name}
-				</h3>
+				<img src={guildObj.image_url ?? sampleIcon} alt="sample-icon" class="rounded-[10px]" />
 			</div>
-			<div class="flex flex-col sm:flex-row gap-[20px]">
-				<!--For guild image-->
+
+			<div class="flex flex-col gap-[10px] w-full">
 				<div class="">
-					<img src={guildObj.image_url ?? sampleIcon} alt="sample-icon" class="rounded-[10px]" />
+					<p class="text-[14px] font-semibold text-main">Host:</p>
+					<p class="text-[14px] text-main">{guildObj.host_name}</p>
 				</div>
 
-				<div class="flex flex-col gap-[10px] w-full">
-					<div class="">
-						<p class="text-[14px] font-semibold text-main">Host:</p>
-						<p class="text-[14px] text-main">{guildObj.host_name}</p>
-					</div>
+				<div class="">
+					<p class="text-[14px] font-semibold text-main">Description:</p>
 
-					<div class="">
-						<p class="text-[14px] font-semibold text-main">Description:</p>
-
-						<p class="text-[14px] text-main line-clamp-3 whitespace-pre-wrap max-w-full break-all">
-							{guildObj.description}
-						</p>
-					</div>
+					<p class="text-[14px] text-main line-clamp-3 whitespace-pre-wrap max-w-full break-all">
+						{guildObj.description}
+					</p>
 				</div>
 			</div>
+		</div>
 
-			<div class="mt-[10px] w-full">
-				<div class="flex items-center justify-between">
-					{#if guildObj.is_private}
-						<div title="This guild is private." class="flex items-center gap-[10px]">
-							<img src={lockIcon} alt="lock-icon" class="" />
-							<p class="text-[14px] text-main">Locked</p>
-						</div>
-					{/if}
-
-					<div class="flex items-center justify-end w-full gap-[5px]">
-						<img src={groupIcon} alt="group-icon" />
-						<p class="text-[14px] text-main">{guildObj.joined_count}/{guildObj.max_users}</p>
+		<div class="absolute bottom-0 left-0 right-0 mb-[10px] px-[20px] w-full">
+			<div class="flex items-center justify-between">
+				{#if guildObj.is_private}
+					<div title="This guild is private." class="flex items-center gap-[10px]">
+						<img src={lockIcon} alt="lock-icon" class="" />
+						<p class="text-[14px] text-main">Locked</p>
 					</div>
+				{/if}
+
+				<div class="flex items-center justify-end w-full gap-[5px]">
+					<img src={groupIcon} alt="group-icon" />
+					<p class="text-[14px] text-main">{guildObj.joined_count}/{guildObj.max_users}</p>
 				</div>
 			</div>
-		</button>
-	</form>
-</div>
+		</div>
+	</button>
+</form>
