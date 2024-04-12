28661d5004a26214dffe041a5f46908e49cc4d4f
itsmikesharescode <mikejohneviota@gmail.com>
2024-04-12 22:06:28 +0800

    update client action guards

--- src/lib/auth-components/projects/join-project/private-join.svelte
+++ src/lib/auth-components/projects/join-project/private-join.svelte
@@ -89,21 +89,34 @@
 				<label>
 					<span class="text-main text-[14px] transition-all">Passcode</span>
 					<input
+						disabled={checkPasscodeLoader}
+						name="passcode"
 						type="password"
 						class="outline-none w-full text-[14px] py-[11px] px-[20px] text-main bg-submain border-[1px] border-main rounded-[10px] transition-all"
 					/>
+					{#each formActionError?.passcode ?? [] as errMsg}
+						<p class="text-main text-[14px]" in:fade>{errMsg}</p>
+					{/each}
 				</label>
 			</div>
 
 			<div class="mt-[30px] flex flex-col gap-[10px]">
 				<button
+					type="submit"
+					disabled={checkPasscodeLoader}
 					on:click={() => ($authState.projects.joinedProject = true)}
-					class="bg-main w-full rounded-[10px] text-[14px] font-semibold py-[10px] px-[2px] flex items-center justify-center text-submain"
+					class="{checkPasscodeLoader ? 'cursor-not-allowed bg-main/50' : 'bg-main'}
+					transition-all active:bg-main/50 w-full rounded-[10px] text-[14px] font-semibold py-[10px] px-[2px] flex items-center justify-center text-submain"
 				>
-					Join
+					{#if checkPasscodeLoader}
+						Joining....
+					{:else}
+						Join
+					{/if}
 				</button>
 
 				<button
+					type="button"
 					on:click={() => (showPrivateJoin = false)}
 					class="bg-submain text-main w-full rounded-[10px] text-[14px] font-semibold py-[8px] px-[2px] flex items-center justify-center border-[1px] border-main"
 				>
